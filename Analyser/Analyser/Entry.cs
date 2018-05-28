ed77fad955d3c7cc73e968a59671f5748dac797c
Kuinox <nvandeginste@gmail.com>
2018-05-28 08:10:18 +0200

    intial push

---
+++ Analyser/Analyser/Entry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analyser
+{
+    class Entry
+    {
+	    public Entry(string textToParse)
+	    {
+		    Id = textToParse.Substring(0, 33);
+		    ConstantInitialData = textToParse.Substring(34, 19);
+		    if (ConstantInitialData != "\v\0\0\0playerElim\0\0\0\0\0")
+		    {
+				Console.WriteLine("Unsupported entry");
+			    return;
+		    }
+		    UnknowData = textToParse.Substring(54, 51);
+		    UserNameLength = (byte) textToParse[105];
+		    try
+		    {
+			    UserName = textToParse.Substring(106+3, UserNameLength);
+			    Data = textToParse.Substring(106 + UserNameLength+3);
+		    }
+		    catch
+		    {
+				Console.WriteLine("ah");
+		    }
+		    
+		    
+
+	    }
+
+		
+	    
+
+	    public string Id { get; set; }
+	    public string ConstantInitialData { get; set; } //19 bytes
+		public string UnknowData { get; set; }//51 bytes
+		public byte UserNameLength { get; set; }
+		public string UserName { get; set; }
+	    public string Data { get; set; }
+	}
+}
---
+++ Analyser/Analyser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Analyser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string saveName = @"UnsavedReplay-2018.05.27-01.26.52";
+			
+			using (var saveFile = File.OpenRead(saveName + ".replay"))
+			{
+				var buffer = new byte[saveFile.Length];
+				saveFile.ReadAsync(buffer, 0, buffer.Length);
+				var saveText = Encoding.ASCII.GetString(buffer);
+				var entriesString = saveText.Split(new[]{ saveName }, StringSplitOptions.None).ToList();
+				var entries = new List<Entry>();
+				var constantData = new List<string>();
+				foreach (var entry in entriesString)
+				{
+					var candidate = new Entry(entry);
+					entries.Add(candidate);
+					constantData.Add(candidate.ConstantInitialData);
+				}
+			}
+		}
+	}
+}
