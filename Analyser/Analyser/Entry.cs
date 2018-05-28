7815c5ec15dfa834490666debbbfa312d224e71c
Kuinox <nvandeginste@gmail.com>
2018-05-29 00:09:05 +0200

    some progress

--- Analyser/Analyser/Entry.cs
+++ Analyser/Analyser/Entry.cs
@@ -1,44 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Analyser
 {
     class Entry
     {
-	    public Entry(string textToParse)
+	    readonly TextReader _textToParse;
+
+	    public Entry(TextReader textToParse)
 	    {
-		    Id = textToParse.Substring(0, 33);
-		    ConstantInitialData = textToParse.Substring(34, 19);
-		    if (ConstantInitialData != "\v\0\0\0playerElim\0\0\0\0\0")
-		    {
-				Console.WriteLine("Unsupported entry");
-			    return;
-		    }
-		    UnknowData = textToParse.Substring(54, 51);
-		    UserNameLength = (byte) textToParse[105];
-		    try
-		    {
-			    UserName = textToParse.Substring(106+3, UserNameLength);
-			    Data = textToParse.Substring(106 + UserNameLength+3);
-		    }
-		    catch
-		    {
-				Console.WriteLine("ah");
-		    }
-		    
-		    
+		    _textToParse = textToParse;
 
 	    }
+	    public static async Task<Entry> GetEntry(TextReader textToParse)
+	    {
+		    var output = new Entry(textToParse);
+		    output.Id = await output.ReadAmount(34);
+		    var typeLength = (byte)await output.ReadSingle();
+		    await output.ReadAmount(3);//Useless data ? nope an int
+		    output.EntryType = await output.ReadAmount(typeLength);
+		    if (output.EntryType != "playerElim\0")
+		    {
+			    Console.WriteLine("Unsupported entry");
+			    return output;
+		    }
 
+		    output.UnknowData = await output.ReadAmount(56);
+
+		    output.UserNameLength = (byte) await output.ReadSingle();
+		    await output.ReadAmount(3); //TODO ???
+		    output.UserName = await output.ReadAmount(output.UserNameLength);
+		    output.SecondUserNameLength = (byte)await output.ReadSingle();
+		    await output.ReadAmount(3); //TODO ???
+			output.SecondUsername = await output.ReadAmount(output.SecondUserNameLength);
+		    return output;
+	    }
+
+	    public async Task<string> ReadAmount(int amountToRead)
+	    {
+		    var buffer = new char[amountToRead];
+		    await _textToParse.ReadAsync(buffer, 0, amountToRead);
+		    return new string(buffer);
+	    }
+
+	    public async Task<char> ReadSingle()
+	    {
+		    return (await ReadAmount(1))[0];
+	    }
 		
 	    
 
 	    public string Id { get; set; }
-	    public string ConstantInitialData { get; set; } //19 bytes
+	    public string EntryType { get; set; } //19 bytes
 		public string UnknowData { get; set; }//51 bytes
 		public byte UserNameLength { get; set; }
 		public string UserName { get; set; }
+		public byte SecondUserNameLength { get; set; }
+		public string SecondUsername { get; set; }
 	    public string Data { get; set; }
 	}
 }
--- Analyser/Analyser/Program.cs
+++ Analyser/Analyser/Program.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Analyser
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			const string saveName = @"UnsavedReplay-2018.05.27-01.26.52";
 			
 			using (var saveFile = File.OpenRead(saveName + ".replay"))
 			{
 				var buffer = new byte[saveFile.Length];
-				saveFile.ReadAsync(buffer, 0, buffer.Length);
+				await saveFile.ReadAsync(buffer, 0, buffer.Length);
 				var saveText = Encoding.ASCII.GetString(buffer);
 				var entriesString = saveText.Split(new[]{ saveName }, StringSplitOptions.None).ToList();
 				var entries = new List<Entry>();
 				var constantData = new List<string>();
 				foreach (var entry in entriesString)
 				{
-					var candidate = new Entry(entry);
+					var candidate = await Entry.GetEntry(new StringReader(entry));
 					entries.Add(candidate);
-					constantData.Add(candidate.ConstantInitialData);
+					constantData.Add(candidate.EntryType);
 				}
 			}
 		}
