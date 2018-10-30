80707333f5ee880195ba7cfd94fa4e4ed6047e13
Kuinox <nvandeginste@gmail.com>
2018-10-30 01:00:22 +0100

    started webapp

---
+++ WebApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new WebHostBuilder()
+                .UseUrls("http://localhost:4324")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseMonitoring()
+                .ConfigureAppConfiguration((hostBuilderContext, confBuilder) =>
+                {
+                    confBuilder
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{hostBuilderContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+
+                })
+                .UseKestrel()
+                .UseStartup<Startup>();
+
+            builder.Build().Run();
+        }
+    }
+}
