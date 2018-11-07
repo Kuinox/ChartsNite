2f16f1431d693a4fd8f7515ca1d801deddd8eebb
Kuinox <nvandeginste@gmail.com>
2018-11-08 00:24:10 +0100

    wip

---
+++ WebApp/Commands/SendReplayCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CK.Crs;
+
+namespace WebApp.Commands
+{
+    class SendReplayCommand : ICommand<SendReplayCommand.Result>
+    {
+
+        class Result
+        {
+
+        }
+    }
+}
---
+++ WebApp/Controllers/DownloadFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("/api/upload")]
+    public class DownloadFile : Controller
+    {
+    }
+}
